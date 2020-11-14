2a81fa9667107227f10c2da0453a65d9ee41d3fd
Mikhail Gorbunov <mihail.grb@gmail.com>
2020-11-15 01:34:04 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.0"
+    id("org.jetbrains.compose") version "0.1.0-m1-build62"
+}
+
+group = "me.sttimort"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+
+    // tests
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "NeBrowserKtApplicationKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "NeBrowserKt"
+        }
+    }
+}
